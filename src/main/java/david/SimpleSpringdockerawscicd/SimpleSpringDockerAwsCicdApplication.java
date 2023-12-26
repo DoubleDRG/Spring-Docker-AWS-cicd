fdde15092361adeb376c4048a3f0bc45d9cb772a
DoubleDRG <dyl660115@gmail.com>
2023-12-27 04:05:53 +0900

    [feat] first init

---
+++ src/main/java/david/SimpleSpringdockerawscicd/SimpleSpringDockerAwsCicdApplication.java
@@ -0,0 +1,13 @@
+package david.SimpleSpringdockerawscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringDockerAwsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringDockerAwsCicdApplication.class, args);
+	}
+
+}
